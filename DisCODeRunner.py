723dd056a5409f781109c36181b480053cd83b02
qiubix <qiubix@gmail.com>
2015-12-16 21:39:00 +0100

    small cleanup

--- DisCODeRunner.py
+++ DisCODeRunner.py
@@ -1,6 +1,6 @@
-import os
-
-
 class DisCODeRunner:
+    def __init__(self):
+        pass
+
     def run(self):
         return "WARNING: Configuration file config.xml not found."
--- DisCODeRunnerTest.py
+++ DisCODeRunnerTest.py
@@ -1,13 +1,14 @@
 import unittest
 from DisCODeRunner import DisCODeRunner
 
-class TestDisCODeRunner(unittest.TestCase):
 
+class TestDisCODeRunner(unittest.TestCase):
     def setUp(self):
         self.runner = DisCODeRunner()
 
     def test_if_discode_installed(self):
         self.assertEqual(self.runner.run(), "WARNING: Configuration file config.xml not found.")
+
 
 if __name__ == '__main__':
     unittest.main()
