ea2fc687d3f3edcdbf9bfb4a3050aa6961bc43fc
qiubix <qiubix@gmail.com>
2015-12-16 21:52:00 +0100

    Test if DisCODe is on PATH

--- DisCODeRunnerTest.py
+++ DisCODeRunnerTest.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from DisCODeRunner import DisCODeRunner
 
@@ -5,6 +6,10 @@ from DisCODeRunner import DisCODeRunner
 class TestDisCODeRunner(unittest.TestCase):
     def setUp(self):
         self.runner = DisCODeRunner()
+
+    def test_discode_exists_on_path(self):
+        path = os.environ['PATH']
+        self.assertEqual("DisCODe" in path, True)
 
     def test_if_discode_installed(self):
         self.assertEqual(self.runner.run(), "WARNING: Configuration file config.xml not found.")
