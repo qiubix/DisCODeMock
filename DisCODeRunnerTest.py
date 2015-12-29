07dc8e47bbba3f97d6d6b15076edd4d5effad02c
qiubix <qiubix@gmail.com>
2015-12-29 12:54:47 +0100

    Manually seach PATH for discode executable

--- DisCODeRunnerTest.py
+++ DisCODeRunnerTest.py
@@ -1,4 +1,3 @@
-import os
 import unittest
 from DisCODeRunner import DisCODeRunner
 
@@ -8,8 +7,20 @@ class TestDisCODeRunner(unittest.TestCase):
         self.runner = DisCODeRunner()
 
     def test_discode_exists_on_path(self):
-        path = os.environ['PATH']
-        self.assertEqual("DisCODe" in path, True)
+        import os
+        discodeExecutable = 'discode'
+
+        def isExe(filepath):
+            return os.path.isfile(filepath) and os.access(filepath, os.X_OK)
+
+        discodeExists = False
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            discodePath = os.path.join(path, discodeExecutable)
+            if isExe(discodePath):
+                discodeExists = True
+
+        self.assertEqual(discodeExists, True)
 
     def test_if_discode_runs(self):
         message = self.runner.run()
