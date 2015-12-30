71e1873c5c90bb546cf64115b140d3549259d78e
qiubix <qiubix@gmail.com>
2015-12-30 10:20:46 +0100

    Launch main program

--- DisCODeRunner.py
+++ DisCODeRunner.py
@@ -9,3 +9,7 @@ class DisCODeRunner:
         process = subprocess.Popen(['discode'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         out, err = process.communicate()
         return out
+
+if __name__ == '__main__':
+    runner = DisCODeRunner()
+    print(runner.run())
