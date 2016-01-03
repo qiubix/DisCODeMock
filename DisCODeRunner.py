abccf12f032401258b05375b9b339bb84f8ab912
qiubix <qiubix@gmail.com>
2016-01-03 14:44:38 +0100

    Code cleanup - get rid of commented out code

--- DisCODeRunner.py
+++ DisCODeRunner.py
@@ -9,7 +9,7 @@ log = ""
 class DisCODeProcess(Thread):
     def run(self):
         global output
-        print("discode starting")
+        # print("discode starting")
         command = ["discode"]
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                    universal_newlines=True)
@@ -19,13 +19,13 @@ class DisCODeProcess(Thread):
             output.append(line)
             condition.notify()
             condition.release()
-            print("DisCODe: ", line)
+            # print("DisCODe: ", line)
 
             if line == "" or "Server stopped." in line:
                 break
 
         process.kill()
-        print("discode ended")
+        # print("discode ended")
 
 
 class OutputMonitor(Thread):
@@ -35,61 +35,17 @@ class OutputMonitor(Thread):
     def run(self):
         global output
         global log
-        print("Monitor started")
+        # print("Monitor started")
         while True:
             condition.acquire()
             if not output:
-                print("Monitor waiting for output")
+                # print("Monitor waiting for output")
                 condition.wait()
             line = output.pop(0)
             log += line
-            print(log)
+            # print(log)
             # print("Monitor: ", line)
             condition.release()
-
-
-# class DisCODeProc:
-#     def __init__(self, output):
-#         threading.Thread.__init__(self)
-        # self.output = output
-        # self.process = None
-        # self.taskName = ""
-        # self.logFileName = ""
-        # self.defaultLogFileName = "output.log"
-        # self.killSignal = False
-
-    # def run(self):
-    #     print("discode starting")
-    #     command = ["discode"]
-    #     if self.taskName != "":
-    #         command = ["discode", "-T" + self.taskName]
-
-        # if self.logFileName == "":
-        #     self.logFileName = self.defaultLogFileName
-
-        # logFile = open(self.logFileName, 'w')
-        # self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-        #                                 universal_newlines=True)
-        # while not self.killSignal:
-        #     line = self.process.stdout.readline()
-        #     output.join(line)
-            # self.output += line
-            # logFile.write(line)
-
-            # if self.killSignal:
-            #     self.process.kill()
-
-            # if line == "" or "Server stopped." in line:
-            #     break
-
-                # if self.terminationStatement == "":
-                #     continue
-
-                # if self.terminationStatement in line:
-                #     self.process.kill()
-
-        # self.process.kill()
-        # print("discode ended")
 
 
 class DisCODeRunner:
@@ -103,26 +59,9 @@ class DisCODeRunner:
         self.log = ""
 
     def run(self):
-        # process = DisCODeProcess(self.output)
-        # process.taskName = self.taskName
-        # process.daemon = True
-        # process.run()
         self.process = DisCODeProcess()
         self.process.daemon = True
         self.process.start()
-        # command = ["discode"]
-        # if self.taskName != "":
-        #     command = ["discode", "-T" + self.taskName]
-        # self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-        #                                 universal_newlines=True)
-        # out, err = process.communicate()
-        # output = process.stdout.readlines()
-
-    def setTerminationFlag(self, terminationStatement):
-        self.terminationStatement = terminationStatement
-
-    def kill(self):
-        self.killSignal = True
 
     def runMonitor(self):
         self.monitor = OutputMonitor()
@@ -132,28 +71,6 @@ class DisCODeRunner:
     def readOutput(self):
         global log
         return log
-        # while True:
-        # line = self.process.stdout.readline()
-        # output.join(line)
-        # output += line
-
-        # if self.killSignal:
-        #     self.process.kill()
-
-        # if line == "" or "Server stopped." in line:
-        #     break
-
-        # if self.terminationStatement == "":
-        #     continue
-
-        # if self.terminationStatement in line:
-        #     self.process.kill()
-
-        # return out
-        # return output.join(self.process.stdout.readlines())
-
-    # def stop(self):
-    #     self.process.
 
 
 if __name__ == '__main__':
