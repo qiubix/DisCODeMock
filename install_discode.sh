beda9182a228803ba468c6cfd3d439802bab5264
qiubix <qiubix@gmail.com>
2016-01-05 13:51:15 +0100

    Install dependencies

--- install_discode.sh
+++ install_discode.sh
@@ -2,9 +2,23 @@
 
 mkdir -p ~/src
 if [ ! -d ~/src/DisCODe ]; then
-  git clone https://github.com/maciek-slon/DisCODe.git ~/src/DisCODe
+  git clone https://github.com/qiubix/DisCODe.git ~/src/DisCODe
   cd ~/src/DisCODe && mkdir build && cd build
   cmake .. -DCMAKE_INSTALL_PREFIX=`pwd`/inst
   make -j4
   make install
 fi
+
+mkdir -p ~/src/DCL
+if [ ! -d ~/src/DCL/CvCoreTypes ]; then
+  git clone https://github.com/maciek-slon/CvCoreTypes.git ~/src/DCL/CvCoreTypes
+  cd ~/src/DCL/CvCoreTypes && mkdir build && cd build
+  cmake .. && make -j3 && make install
+fi
+
+if [ ! -d ~/src/DCL/CvBasic ]; then
+  git clone https://github.com/DisCODe/DCL_CvBasic ~/src/DCL/CvBasic
+  cd ~/src/DCL/CvBasic && mkdir build && cd build
+  cmake .. -DCMAKE_SKIP_INSTALL_ALL_DEPENDENCY=true && make Sequence CvSIFT -j3 && make install/fast
+fi
+
