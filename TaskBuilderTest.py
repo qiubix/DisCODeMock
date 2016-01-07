075b8038e2657813e9458e48f3d2af091b7954c6
qiubix <qiubix@gmail.com>
2016-01-07 13:39:59 +0100

    Refactor tests for TaskBuilder

--- TaskBuilderTest.py
+++ TaskBuilderTest.py
@@ -6,26 +6,28 @@ from TaskBuilder import TaskBuilder
 
 
 class TaskBuilderTest(unittest.TestCase):
+    def setUp(self):
+        self.defaultFileName = 'test_tasks/test_task.xml'
+        self.builder = TaskBuilder()
+
     def test_should_create_file_with_specific_name(self):
         file_name = 'test_tasks/sample_test_task.xml'
         sample_string = 'sample string'
         call(['rm', 'test_tasks/sample_test_task.xml'])
-        builder = TaskBuilder()
-        builder.fileName = file_name
+        self.builder.fileName = file_name
 
-        builder.writeToFile(sample_string)
+        self.builder.writeToFile(sample_string)
 
         file = open(file_name)
         assert_that(file.read(), sample_string)
 
     def test_should_save_task_to_default_file(self):
-        defaultFileName = 'test_tasks/test_task.xml'
         taskBody = 'Task body'
-        builder = TaskBuilder()
-        builder.taskBody = taskBody
+        self.builder.taskBody = taskBody
 
-        builder.save()
-        file = open(defaultFileName)
+        self.builder.save()
+
+        file = open(self.defaultFileName)
         assert_that(file.read(), taskBody)
 
 
