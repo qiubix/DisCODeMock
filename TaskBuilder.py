6d367bb3da48e88e51ca34dc3b52a7641f412c81
qiubix <qiubix@gmail.com>
2016-01-07 13:37:35 +0100

    Save task to default file

--- TaskBuilder.py
+++ TaskBuilder.py
@@ -1,7 +1,16 @@
 class TaskBuilder:
     def __init__(self):
         self.fileName = ''
+        self.taskBody = ''
 
     def writeToFile(self, string):
         file = open(self.fileName, 'w')
         file.write(string)
+
+    def save(self):
+        if self.fileName == '':
+            fileName = 'test_tasks/test_task.xml'
+        else:
+            fileName = self.fileName
+        file = open(fileName, 'w')
+        file.write(self.taskBody)
--- TaskBuilderTest.py
+++ TaskBuilderTest.py
@@ -1,14 +1,15 @@
 import unittest
 from hamcrest import *
 from subprocess import call
+
 from TaskBuilder import TaskBuilder
 
 
 class TaskBuilderTest(unittest.TestCase):
     def test_should_create_file_with_specific_name(self):
-        file_name = 'test_tasks/test_task.xml'
+        file_name = 'test_tasks/sample_test_task.xml'
         sample_string = 'sample string'
-        call(['rm', 'test_tasks/test_task.xml'])
+        call(['rm', 'test_tasks/sample_test_task.xml'])
         builder = TaskBuilder()
         builder.fileName = file_name
 
@@ -16,6 +17,16 @@ class TaskBuilderTest(unittest.TestCase):
 
         file = open(file_name)
         assert_that(file.read(), sample_string)
+
+    def test_should_save_task_to_default_file(self):
+        defaultFileName = 'test_tasks/test_task.xml'
+        taskBody = 'Task body'
+        builder = TaskBuilder()
+        builder.taskBody = taskBody
+
+        builder.save()
+        file = open(defaultFileName)
+        assert_that(file.read(), taskBody)
 
 
 if __name__ == '__main__':
