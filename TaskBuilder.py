7899ffbdf1ef274e08bdd5dddf9bff2d6ae50517
qiubix <qiubix@gmail.com>
2016-01-08 15:11:49 +0100

    Refactor createTemplate method

--- TaskBuilder.py
+++ TaskBuilder.py
@@ -19,18 +19,33 @@ class TaskBuilder:
         file.write(self.taskBody)
 
     def createTemplate(self):
+        document = self.createEmptyDocument()
+        topLevelElement = self.getTopLevelElement(document)
+        subtasksElement = self.addSubtasksElement(document, topLevelElement)
+        self.addDataStreams(document, topLevelElement)
+        self.addMainSubtask(document, subtasksElement)
+
+        self.taskBody = document.firstChild.toprettyxml()
+
+    def getTopLevelElement(self, document):
+        topLevelElement = document.documentElement
+        return topLevelElement
+
+    def createEmptyDocument(self):
         DOMimpl = getDOMImplementation()
         document = DOMimpl.createDocument(None, 'Task', None)
-        topLevelElement = document.documentElement
+        return document
 
-        subtasksElement = document.createElement('Subtasks')
-        topLevelElement.appendChild(subtasksElement)
+    def addDataStreams(self, document, topLevelElement):
+        datastreamsElement = document.createElement('DataStreams')
+        topLevelElement.appendChild(datastreamsElement)
+
+    def addMainSubtask(self, document, subtasksElement):
         mainSubtaskElement = document.createElement('Subtask')
         mainSubtaskElement.setAttribute('name', 'Main')
         subtasksElement.appendChild(mainSubtaskElement)
 
-        datastreamsElement = document.createElement('DataStreams')
-        topLevelElement.appendChild(datastreamsElement)
-
-        self.taskBody = document.firstChild.toprettyxml()
-        # self.taskBody = '<Task>\n<Subtasks>\n</Subtasks>\n<Datasets>\n</Datasets>\n</Task>'
+    def addSubtasksElement(self, document, topLevelElement):
+        subtasksElement = document.createElement('Subtasks')
+        topLevelElement.appendChild(subtasksElement)
+        return subtasksElement
