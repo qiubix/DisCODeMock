63baf7cd2625376fa73c1b6746f651be10e8b019
qiubix <qiubix@gmail.com>
2016-01-16 12:55:52 +0100

    Add proper component to task

--- ComponentTester.py
+++ ComponentTester.py
@@ -1,2 +1,12 @@
+from TaskBuilder import TaskBuilder
+
+
 class ComponentTester:
-    pass
+    def __init__(self):
+        self.taskBuilder = TaskBuilder()
+
+    def setComponent(self, componentName, componentType):
+        self.taskBuilder.createTemplate()
+        self.taskBuilder.addDefaultExecutor()
+        self.taskBuilder.addComponent(componentName,componentType)
+        self.taskBuilder.save()
--- ComponentTesterTest.py
+++ ComponentTesterTest.py
@@ -5,8 +5,19 @@ from ComponentTester import ComponentTester
 
 
 class TestComponentTester(unittest.TestCase):
+    def setUp(self):
+        self.defaultFileName = 'test_tasks/test_task.xml'
+
     def test_component_tester_running(self):
         assert_that(ComponentTester(), is_not(None))
+
+    def test_adds_proper_component_to_task(self):
+        tester = ComponentTester()
+        tester.setComponent('Summator', 'CvBasic:Sum')
+
+        with open(self.defaultFileName) as file:
+            contents = file.read()
+        assert_that(contents, contains_string('<Component bump="0" name="Summator" priority="1" type="CvBasic:Sum"/>'))
 
 
 if __name__ == '__main__':
